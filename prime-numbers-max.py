e5daf220d5214d36b2cb8c555d01ef2825966fb7
hunterhall5104 <113056759+hunterhall5104@users.noreply.github.com>
2022-09-07 14:08:20 -0500

    Create prime-numbers-max.py

---
+++ prime-numbers-max.py
@@ -0,0 +1,13 @@
+#Prime numbers up to x
+num = int(input("Input number of prime numbers: "))
+lst_of_primes = [1, ]
+prime = True
+for x in range(2, num):
+    for i in range(2, x):
+        if (x % i) == 0:
+            prime = False
+            break
+    if prime:
+        lst_of_primes.append(x)
+    prime = True
+print(lst_of_primes)
